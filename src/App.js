58aa099ca7cb5ce473f2a52d37507a0e2407ab01
rilex001 <ristic1900@gmail.com>
2020-06-14 21:49:49 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'semantic-ui-react'
+import { Card, Icon, Image } from 'semantic-ui-react'
+import { Dropdown } from 'semantic-ui-react'
+
+
+const countryOptions = [
+  { key: 'af', value: 'af', flag: 'af', text: 'Afghanistan' },
+  { key: 'ax', value: 'ax', flag: 'ax', text: 'Aland Islands' },
+  { key: 'al', value: 'al', flag: 'al', text: 'Albania' },
+  { key: 'dz', value: 'dz', flag: 'dz', text: 'Algeria' },
+  { key: 'as', value: 'as', flag: 'as', text: 'American Samoa' },
+  { key: 'ad', value: 'ad', flag: 'ad', text: 'Andorra' },
+  { key: 'ao', value: 'ao', flag: 'ao', text: 'Angola' },
+  { key: 'ai', value: 'ai', flag: 'ai', text: 'Anguilla' },
+  { key: 'ag', value: 'ag', flag: 'ag', text: 'Antigua' },
+  { key: 'ar', value: 'ar', flag: 'ar', text: 'Argentina' },
+  { key: 'am', value: 'am', flag: 'am', text: 'Armenia' },
+  { key: 'aw', value: 'aw', flag: 'aw', text: 'Aruba' },
+  { key: 'au', value: 'au', flag: 'au', text: 'Australia' },
+  { key: 'at', value: 'at', flag: 'at', text: 'Austria' },
+  { key: 'az', value: 'az', flag: 'az', text: 'Azerbaijan' },
+  { key: 'bs', value: 'bs', flag: 'bs', text: 'Bahamas' },
+  { key: 'bh', value: 'bh', flag: 'bh', text: 'Bahrain' },
+  { key: 'bd', value: 'bd', flag: 'bd', text: 'Bangladesh' },
+  { key: 'bb', value: 'bb', flag: 'bb', text: 'Barbados' },
+  { key: 'by', value: 'by', flag: 'by', text: 'Belarus' },
+  { key: 'be', value: 'be', flag: 'be', text: 'Belgium' },
+  { key: 'bz', value: 'bz', flag: 'bz', text: 'Belize' },
+  { key: 'bj', value: 'bj', flag: 'bj', text: 'Benin' },
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>Click me</Button>
+      <Button circular color='facebook' icon='facebook' />
+      <Button circular color='twitter' icon='twitter' />
+      <Button circular color='linkedin' icon='linkedin' />
+      <Button circular color='google plus' icon='google plus' />
+  
+
+    <Card>
+      <Image src='slika.jpg' wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>Matthew</Card.Header>
+            <Card.Meta>
+              <span className='date'>Joined in 2015</span>
+            </Card.Meta>
+            <Card.Description>
+              Matthew is a musician living in Nashville.
+            </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a>
+            <Icon name='user' />
+              22 Friends
+          </a>
+        </Card.Content>
+    </Card>
+
+    <Dropdown
+    placeholder='Select Country'
+    fluid
+    search
+    selection
+    options={countryOptions}
+  />
+
     </div>
   );
 }
