7df959ac5da4d20207188758f1c65e133d017c8f
rilex001 <ristic1900@gmail.com>
2020-06-15 16:37:20 +0200

    Team

--- src/component/Team.jsx
+++ src/component/Team.jsx
@@ -1,15 +1,41 @@
 import React from 'react'
-
+import picture2 from '../img/picture2.jpg'
+import { Grid, Button } from 'semantic-ui-react'
 
 
 
 function Team() {
     return (
         <div className='container'>
-            
-                        <img src='https://via.placeholder.com/600/24f355' alt='mtk'>
-                        </img>                    
+            <Grid columns={4} divided inverted>
+                <Grid.Row color='white' textAlign='center'>
+                    <Grid.Column>
+                            
+                    </Grid.Column>
+
+                    <Grid.Column>
+                    <img src={picture2} className='header' />
+                    </Grid.Column>
+
+                    <Grid.Column>
+                    <img src={picture2} className='header' />
+                    </Grid.Column>
+
+                    <Grid.Column>
+                        
+                    </Grid.Column>
                     
+                </Grid.Row>
+
+
+            </Grid>
+                         
+          <div className='headercontainer'>             
+            <p className='microsoftteam'><b>Microsoft Teams - bezbedan</b></p> 
+            <p className='microsoftteam'><b> Bezbedan. Besplatan</b></p>
+            <p className='microsoftteaminfo'><b>Teams vam moze pomoci da ostanete produktivni, bez obzira na to gde se nalazite</b></p >
+            <Button size='large'>Saznajte vise > </Button>
+            </div>         
         </div>
     )
 }
